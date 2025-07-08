b16a2090fb25a86ab7fbfdd0f4b7bcea860ba5cf
Bruno Piovan <bpiovan90@gmail.com>
2025-07-08 20:23:55 -0300

    adicionada primeiras rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/menu/nav/nav.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:NavComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
