4eba6859a9fc1dc956aecf8087025b5639a8474c
Bruno Piovan <bpiovan90@gmail.com>
2025-07-08 20:18:04 -0300

    adicionado angular material ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -14,7 +30,24 @@ import { RegisterComponent } from './components/auth/register/register.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+     // Angular Material
+     MatFormFieldModule,
+     MatPaginatorModule,
+     MatCheckboxModule,
+     MatSnackBarModule,
+     MatToolbarModule,
+     MatSidenavModule,
+     MatButtonModule,
+     MatSelectModule,
+     MatInputModule,
+     MatRadioModule,
+     MatTableModule,
+     MatIconModule,
+     MatListModule,
+     MatCardModule,
+     MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
