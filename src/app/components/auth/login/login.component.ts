ecf0d3a45d455f476c66c33b7c655ec625c25e16
Bruno Piovan <bpiovan90@gmail.com>
2025-07-08 21:28:16 -0300

    html e css basico para tela de login e registro

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  hidePassword = true;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Login attempt:', username, password);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+  get password() {
+    return this.loginForm.get('password');
   }
 
 }
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+   loginForm!: FormGroup;
+   hidePassword = true;
+   hidePassword2 = true;
 
-  ngOnInit(): void {
-  }
+   constructor(private fb: FormBuilder) {}
 
+   ngOnInit(): void {
+     this.loginForm = this.fb.group({
+       username: ['', [Validators.required, Validators.minLength(3)]],
+       password: ['', [Validators.required, Validators.minLength(6)]],
+       confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+     });
+   }
+
+   onSubmit(): void {
+     if (this.loginForm.valid) {
+       const { username, password } = this.loginForm.value;
+       console.log('Login attempt:', username, password);
+     } else {
+       this.loginForm.markAllAsTouched();
+     }
+   }
+
+   get username() {
+     return this.loginForm.get('username');
+   }
+   get password() {
+     return this.loginForm.get('password');
+   }
+
+   get confirmPassword() {
+     return this.loginForm.get('confirmPassword');
+   }
 }
